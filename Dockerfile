33966ecbafbc42793cb267a459934dceb375f9ab
blublinsky <boris.lublinsky@lightbend.com>
2019-02-20 17:59:46 -0600

    Clean up

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,6 @@
 FROM flink:1.7.1-scala_2.11
-COPY ./prometheus/flink-metrics-prometheus_2.11-1.7.1.jar /opt/flink/lib
+
+# Add Prometheus, Table and Queryable state jars. Add additional jars if you need
+RUN mv /opt/flink/opt/flink-metrics-prometheus-1.7.1.jar /opt/flink/lib/
+RUN mv /opt/flink/opt/flink-queryable-state-runtime_2.11-1.7.1.jar /opt/flink/lib/
+RUN mv /opt/flink/opt/flink-table_2.11-1.7.1.jar /opt/flink/lib/
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
 
-###############################################################################
-#  Licensed to the Apache Software Foundation (ASF) under one
-#  or more contributor license agreements.  See the NOTICE file
-#  distributed with this work for additional information
-#  regarding copyright ownership.  The ASF licenses this file
-#  to you under the Apache License, Version 2.0 (the
-#  "License"); you may not use this file except in compliance
-#  with the License.  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-# limitations under the License.
-###############################################################################
+################################################################################
+#   from https://github.com/apache/flink/blob/release-1.7/flink-container/docker/docker-entrypoint.sh
+#   and https://github.com/docker-flink/docker-flink/blob/63b19a904fa8bfd1322f1d59fdb226c82b9186c7/1.7/scala_2.11-alpine/docker-entrypoint.sh
+################################################################################
 
 # If unspecified, the hostname of the container is taken as the JobManager address
 JOB_MANAGER_RPC_ADDRESS=${JOB_MANAGER_RPC_ADDRESS:-$(hostname -f)}
@@ -34,29 +21,34 @@ drop_privs_cmd() {
     fi
 }
 
-if [ "$1" = "help" ]; then
-    echo "Usage: $(basename "$0") (jobmanager|taskmanager|help)"
+JOB_MANAGER="jobmanager"
+TASK_MANAGER="taskmanager"
+
+CMD="$1"
+shift
+
+if [ "${CMD}" = "help" ]; then
+    echo "Usage: $(basename $0) (${JOB_MANAGER}|${TASK_MANAGER}|help)"
     exit 0
-elif [ "$1" = "jobmanager" ]; then
-    shift 1
-    echo "Starting Job Manager"
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
+elif [ "${CMD}" = "${JOB_MANAGER}" -o "${CMD}" = "${TASK_MANAGER}" ]; then
+    if [ "${CMD}" = "${TASK_MANAGER}" ]; then
+        TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
 
-    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
-    exec $(drop_privs_cmd) "$FLINK_HOME/bin/jobmanager.sh" start-foreground "$@"
-elif [ "$1" = "taskmanager" ]; then
-    TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
+        sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+        sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
 
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
-    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
-    echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
-
-    echo "Starting Task Manager"
-    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
-    exec $(drop_privs_cmd) "$FLINK_HOME/bin/taskmanager.sh" start-foreground
+        echo "Starting Task Manager"
+        echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+        exec $(drop_privs_cmd) "$FLINK_HOME/bin/taskmanager.sh" start-foreground
+    else
+        sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+        exec $(drop_privs_cmd) "$FLINK_HOME/bin/jobmanager.sh" start-foreground "$@"
+    fi
 fi
 
 exec "$@"
