0b5bf0212852d762524ae7ea481d74a5fc3658cc
blublinsky <boris.lublinsky@lightbend.com>
2019-02-22 11:56:54 -0600

    Updates to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM flink:1.7.1-scala_2.11
+FROM flink:1.7.2-scala_2.11
 
 # Add Prometheus, Table and Queryable state jars. Add additional jars if you need
-RUN mv /opt/flink/opt/flink-metrics-prometheus-1.7.1.jar /opt/flink/lib/
-RUN mv /opt/flink/opt/flink-queryable-state-runtime_2.11-1.7.1.jar /opt/flink/lib/
-RUN mv /opt/flink/opt/flink-table_2.11-1.7.1.jar /opt/flink/lib/
+RUN mv /opt/flink/opt/flink-metrics-prometheus-1.7.2.jar /opt/flink/lib/
+RUN mv /opt/flink/opt/flink-queryable-state-runtime_2.11-1.7.2.jar /opt/flink/lib/
+RUN mv /opt/flink/opt/flink-table_2.11-1.7.2.jar /opt/flink/lib/
+
+# Update entrypoint
+COPY docker-entrypoint.sh /
+RUN chmod +x /docker-entrypoint.sh /
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,6 @@
 #   and https://github.com/docker-flink/docker-flink/blob/63b19a904fa8bfd1322f1d59fdb226c82b9186c7/1.7/scala_2.11-alpine/docker-entrypoint.sh
 ################################################################################
 
-# If unspecified, the hostname of the container is taken as the JobManager address
-JOB_MANAGER_RPC_ADDRESS=${JOB_MANAGER_RPC_ADDRESS:-$(hostname -f)}
-
 drop_privs_cmd() {
     if [ $(id -u) != 0 ]; then
         # Don't need to drop privs if EUID != 0
@@ -25,27 +22,34 @@ JOB_MANAGER="jobmanager"
 TASK_MANAGER="taskmanager"
 
 CMD="$1"
-shift
+
 
 if [ "${CMD}" = "help" ]; then
     echo "Usage: $(basename $0) (${JOB_MANAGER}|${TASK_MANAGER}|help)"
     exit 0
 elif [ "${CMD}" = "${JOB_MANAGER}" -o "${CMD}" = "${TASK_MANAGER}" ]; then
+    shift
+    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "metrics.reporters: prom" | tee -a "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "metrics.reporter.prom.class: org.apache.flink.metrics.prometheus.PrometheusReporter" | tee -a "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "metrics.reporter.prom.port: 9249" | tee -a "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "metrics.internal.query-service.port: ${CONTAINER_METRIC_PORT}" | tee -a "$FLINK_HOME/conf/flink-conf.yaml"
     if [ "${CMD}" = "${TASK_MANAGER}" ]; then
         TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
 
-        sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "Starting Task Manager"
         sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "taskmanager.host : ${K8S_POD_IP}" | tee -a "$FLINK_HOME/conf/flink-conf.yaml"
         echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
         echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
 
-        echo "Starting Task Manager"
         echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
         exec $(drop_privs_cmd) "$FLINK_HOME/bin/taskmanager.sh" start-foreground
     else
-        sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+        echo "Starting Job Manager"
         echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
         echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
+
         echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
         exec $(drop_privs_cmd) "$FLINK_HOME/bin/jobmanager.sh" start-foreground "$@"
     fi
